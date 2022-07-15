ab355082edff674ccd8c5cfd422e2a854d18c224
jayampathi-bac <jayampathi.bac@gmail.com>
2022-07-15 16:08:18 +0530

    initial commit

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const { s3UploadV2 } = require("./s3Service");
+const multer = require("multer");
 
 app.get("/", (req, res, next) => {
   return res.status(200).json({
@@ -12,6 +15,35 @@ app.get("/hello", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
+});
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.split("/")[0] === "image") {
+    cb(null, true);
+  } else {
+    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
+  }
+};
+
+// ["image", "jpeg"]
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 1000000000, files: 2 },
+});
+
+
+app.post("/upload", upload.array("file"), async (req, res) => {
+  try {
+    const results = await s3UploadV2(req.files);
+    console.log(results);
+    return res.json({ status: "success" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.use((req, res, next) => {
